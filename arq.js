817f95c1f24f87035e5749641ee4078e4c8bd125
ThassiaYmara <thassiaymarasantos@gmail.com>
2019-01-03 21:50:58 -0200

    Sua mensagem aqui

---
+++ arq.js
@@ -0,0 +1,9 @@
+
+function login() {
+	
+	let email = document.getElementById("email").value;
+	let senha = document.getElementById("password").value;
+	
+	console.log("email: " + email + "senha: " + senha);
+	
+}
